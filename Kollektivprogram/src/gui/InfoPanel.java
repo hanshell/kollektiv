0ffb77712977a5bc33033347ff5444c77efaa212
hanshell <hanshmelby@gmail.com>
2013-09-23 00:41:38 +0200

    Added ListSelectionListener to personlist to update textfields

--- Kollektivprogram/src/gui/InfoPanel.java
+++ Kollektivprogram/src/gui/InfoPanel.java
@@ -5,36 +5,42 @@ import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class InfoPanel extends JPanel{
+import model.Person;
+
+public class InfoPanel extends JPanel {
 	private JLabel personInfoLabel;
 	private JLabel nameLabel;
 	private JLabel fieldOfStudyLabel;
 	private JLabel emailLabel;
 	private JLabel phoneNumberLabel;
 	private JLabel genderLabel;
-	
-	private JTextField name;
-	private JTextField fieldOfStudy;
-	private JTextField email;
-	private JTextField phoneNumber;
-	private JTextField gender;
-	
+
+	static JTextField name;
+	static JTextField fieldOfStudy;
+	static JTextField email;
+	static JTextField phoneNumber;
+	static JTextField gender;
+
 	public InfoPanel(){
 		//setting layout
 		this.setLayout(new GridBagLayout());
-		
+
 		this.setBorder(BorderFactory.createTitledBorder(null, "Person-info", TitledBorder.CENTER, TitledBorder.CENTER));
-		
-		
+
+
 		this.setBackground(Color.YELLOW);
-		
+
 		GridBagConstraints gbc=new GridBagConstraints();
 
 		//instantiating labels
@@ -43,7 +49,7 @@ public class InfoPanel extends JPanel{
 		emailLabel=new JLabel("E-mail: ");
 		phoneNumberLabel=new JLabel("Tlf. nr: ");
 		genderLabel=new JLabel("Gender: ");
-		
+
 		//instantiating textfields
 		name=new JTextField(15);
 		fieldOfStudy=new JTextField(15);
@@ -51,60 +57,77 @@ public class InfoPanel extends JPanel{
 		phoneNumber=new JTextField(15);
 		gender=new JTextField(15);
 		
+		try{	
+		setTextFields();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+
 		//setting GridBagConstraint-values and adding to inherited JPanel
-				
+
 		gbc.anchor=GridBagConstraints.NORTHWEST;
-		
+
 		gbc.weightx=0.5;
 		gbc.weighty=0.5;
-		
+
 		gbc.insets=new Insets(30, 50, 30, 0);
-		
+
 		gbc.gridx=0;
 		gbc.gridy=0;
 		this.add(nameLabel, gbc);
-		
-		
+
+
 		gbc.gridx=1;
 		gbc.gridy=0;
 		this.add(name, gbc);
-		
+
 		gbc.gridx=0;
 		gbc.gridy=1;
 		add(fieldOfStudyLabel, gbc);
-		
+
 		gbc.gridx=1;
 		gbc.gridy=1;
 		add(fieldOfStudy, gbc);
-		
+
 		gbc.gridx=0;
 		gbc.gridy=2;
 		add(emailLabel, gbc);
-		
+
 		gbc.gridx=1;
 		gbc.gridy=2;
 		add(email, gbc);
-		
+
 		gbc.gridx=0;
 		gbc.gridy=3;
 		add(phoneNumberLabel, gbc);
-		
+
 		gbc.gridx=1;
 		gbc.gridy=3;
 		add(phoneNumber, gbc);
-		
-		
+
+
 		gbc.gridx=0;
 		gbc.gridy=4;
 		add(genderLabel, gbc);
-		
+
 		gbc.gridx=1;
 		gbc.gridy=4;
 		add(gender, gbc);
-		
+
 		for(Component c: getComponents())
 			if(c instanceof JTextField)
 				((JTextField) c).setEditable(false); //Sets every JTextField-editable field to false
+	}
+	private void setTextFields() throws SQLException{
+		String person=PersonListPanel.getPersonlist().getSelectedValue();
+		ArrayList<Person> persons=Person.getPersonList();
 		
+		for(int i=0; i<persons.size(); i++){
+			if((persons.get(i).getFirstName()+" "+persons.get(i).getLastName()).equals(person)){
+				name.setText(person);
+				fieldOfStudy.setText(persons.get(i).getFieldOfStudy());
+				
+			}
+		}
 	}
 }
--- Kollektivprogram/src/gui/PersonListPanel.java
+++ Kollektivprogram/src/gui/PersonListPanel.java
@@ -16,6 +16,7 @@ import java.awt.Insets;
 import java.awt.LayoutManager;
 import java.awt.SecondaryLoop;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -26,17 +27,18 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class PersonListPanel extends JPanel {
-	private JList<String> personlist;
+	private static JList<String> personlist;
 	private JButton addPerson;
 	private JScrollPane scrollpane;
 	
 	public PersonListPanel(){
 		setLayout(new GridBagLayout());
 		setBorder(BorderFactory.createTitledBorder("Navn: "));
-		
-//		String[] names={"Hans Melby", "Anne Sofie Solberg", "Martin Ervik", "Tormod Bjørnhaug"};
+
 		String[] names=null;
 		try {
 			names = getPersonList();
@@ -61,6 +63,32 @@ public class PersonListPanel extends JPanel {
 		scrollpane=new JScrollPane(personlist);
 		add(scrollpane, gbc);
 		
+		/*
+		 * ListSelectionListener to listen to selection-changes in personList
+		 * in order to change appropriate JTextField-values in InfoPanel
+		 */
+		personlist.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent arg0) {
+				String person=getPersonlist().getSelectedValue();
+				ArrayList<Person> persons=null;
+				try {
+					persons = Person.getPersonList();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				for(int i=0; i<persons.size(); i++){
+					if((persons.get(i).getFirstName()+" "+persons.get(i).getLastName()).equals(person)){
+						InfoPanel.name.setText(person);
+						InfoPanel.fieldOfStudy.setText(persons.get(i).getFieldOfStudy());
+						
+					}
+				}
+			}
+		});
+		
 		gbc.anchor=GridBagConstraints.SOUTH;
 		gbc.fill=GridBagConstraints.SOUTH;
 		gbc.gridx=0;
@@ -75,5 +103,8 @@ public class PersonListPanel extends JPanel {
 			names[i]=Person.getNames().get(i);
 		}
 		return names;
+	}
+	public static JList<String> getPersonlist(){
+		return personlist;
 	}
 }

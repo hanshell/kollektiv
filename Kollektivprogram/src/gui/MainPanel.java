f7a30e54850ff3cee052ae85d8598f45e38247b5
hanshell <hanshmelby@gmail.com>
2013-09-22 20:04:25 +0200

    DBConnection.connect() is called in the beginning of the constructor

--- Kollektivprogram/src/gui/MainPanel.java
+++ Kollektivprogram/src/gui/MainPanel.java
@@ -1,20 +1,27 @@
 package gui;
 
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.Point;
+
 import java.awt.BorderLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+
+import db.DBConnection;
+
+/*
+ * Sets Panel-layout of main JFrame, adds other panels to main panel
+ */
 
 public class MainPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private static JFrame frame;
 	public MainPanel(){
+		DBConnection.connect();
+		
 		setLayout(new BorderLayout());
 		add(new PersonListPanel(), BorderLayout.WEST);
 		add(new InfoPanel(), BorderLayout.CENTER);
@@ -24,7 +31,7 @@ public class MainPanel extends JPanel {
 	public static void disposeFrame(){
 		frame.dispose();
 	}
-	public static void s(){ //displays main frame. 's' is short for 'show', as the latter is a reserved java method name
+	public static void showMainFrame() { //displays main frame
 		frame=new JFrame("Kollektivapplikasjon");
 		frame.setSize(new Dimension(1000, 600));
 		frame.setResizable(true);
